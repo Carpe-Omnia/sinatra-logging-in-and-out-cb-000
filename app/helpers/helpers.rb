e958af65f2b7822de1f5c669830d35c377f6ca7d
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-15 01:05:05 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,7 @@
 class Helpers
   def self.current_user(session)
     user_id = session[:id]
-    user = User.find(user_id)
+    user = User.find(session[:id])
     user
   end
   def self.is_logged_in?(session)
