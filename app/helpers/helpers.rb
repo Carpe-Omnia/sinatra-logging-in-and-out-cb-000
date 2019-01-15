f68d0d9905b7581ecae4ecaf7998ddcc324eb928
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-15 01:09:15 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,7 @@
 class Helpers
   def self.current_user(sesh)
-    user_id = sesh[:id]
-    user = User.find(sesh[:id])
+    user_id = sesh[:user_id]
+    user = User.find(sesh[:user_id])
     user
   end
   def self.is_logged_in?(session)
