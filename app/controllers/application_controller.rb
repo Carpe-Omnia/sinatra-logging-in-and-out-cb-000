f1109412ce487db6184ee455c49c0d83f5f27182
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-15 01:36:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     user = User.find(session[:user_id])
-    if @user != nil
+    if user != nil
       @sesh = session
       erb :account
     else
